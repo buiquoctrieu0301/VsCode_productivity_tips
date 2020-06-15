52be8480f65752e19de0a37efd31ecf5b3dbe601
buiquoctrieu0301 <61806789+buiquoctrieu0301@users.noreply.github.com>
2020-06-15 11:54:21 +1000

    Add files via upload

---
+++ Exercise File/Refactor1.js
@@ -0,0 +1,13 @@
+function CalculatePaintNeeded(paintPerUnit, radius) {
+  // rename symbol
+  // extract constant
+  // extract method
+
+
+
+  var ar;
+  ar = 3.141592 * radius * radius;
+
+  return ar / paintPerUnit;
+
+}
